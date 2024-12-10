32b4b6b4673603ada8326017eb602b3526cee982
Carter Solberg <csolbs24@gmail.com>
2024-12-09 18:41:22 -0700

    Fix import error

--- database.js
+++ database.js
@@ -3,12 +3,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.1/firebase
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-firestore.js"
 import { collection, getDocs, addDoc, Timestamp } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-firestore.js"
 import { query, orderBy, limit, where, onSnapshot } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-firestore.js"
-import { api_key } from "./api_key";
+import { api_key } from "./api_key.js";
 
 // Our web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: api_key, // DO NOT COMMIT THIS!
+  apiKey: api_key, // DO NOT COMMIT THE API KEY!
   authDomain: "cse310-shared-calendar.firebaseapp.com",
   projectId: "cse310-shared-calendar",
   storageBucket: "cse310-shared-calendar.firebasestorage.app",
