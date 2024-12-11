d0af0b5ef1642789b8ef964302ea11e1386286a1
Carter Solberg <csolbs24@gmail.com>
2024-12-10 23:54:11 -0700

    Init basic app with webpack

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+import { api_key } from "./api_key";
+
+const firebaseConfig = {
+  apiKey: api_key, // DO NOT COMMIT THE API KEY!
+  authDomain: "cse310-shared-calendar.firebaseapp.com",
+  projectId: "cse310-shared-calendar",
+  storageBucket: "cse310-shared-calendar.firebasestorage.app",
+  messagingSenderId: "102136831528",
+  appId: "1:102136831528:web:51d45e19d0620048555f1d",
+  measurementId: "G-BVS3SYJZZ6",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseApp);
+const db = getFirestore(firebaseApp);
